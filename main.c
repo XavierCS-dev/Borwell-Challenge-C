6631cec0fbfecf6a7cd1465a78315b78cf0a6d64
XavierCyber <xavierelwell@gmail.com>
2021-11-20 20:14:46 +0000

    Enforced walls to conform to floor, fixed some input bugs and code styling, compiled with c17 and O3

--- main.c
+++ main.c
@@ -2,12 +2,14 @@
 #include <stdlib.h>
 
 
+
+// Pointer to variable required, to change value of local var in main
 void get_input(char *message, double *value_to_store)
 {
     _Bool input_valid = 0;
     while(!input_valid)
     {
-        printf("Please enter %s: ", message);
+        printf("Please enter %s (metres): ", message);
         if(!scanf("%lf", value_to_store))
         {
             puts("Invalid value, please enter a number");
@@ -17,7 +19,7 @@ void get_input(char *message, double *value_to_store)
             while((c = getchar()) != EOF && c != '\n') {}
         } else
         {
-            printf("You entered: %.2lf\n", *value_to_store);
+            printf("You entered: %.2lfm\n", *value_to_store);
             input_valid = 1;
         }
     }
@@ -36,18 +38,19 @@ int main(void)
     floor_area = floor_length * floor_width;
     get_input("room height", &room_height);
     room_volume = floor_area * room_height;
-    get_input("wall one length", &wall_one_length);
-    get_input("wall two length", &wall_two_length);
-    get_input("wall three length", &wall_three_length);
-    get_input("wall four length", &wall_four_length);
+
+    // Automatically calculate wall lengths to enforce fully enclosed rooms.
+    wall_one_length = floor_length;
+    wall_two_length = floor_length;
+    wall_three_length = floor_width; 
+    wall_four_length = floor_width;
 
     // Total surface area calculated by adding surface area of all walls.
     // This can be done seperately, however I have shortened it using a distributivity law
     paint_needed = (wall_one_length + wall_two_length + wall_three_length + wall_four_length) * room_height;
-    printf("The area of the floor is %.2lf\n", floor_area);
-    printf("The amount of paint needed to paint the walls is: %.2lf\n", paint_needed);
-    printf("The volume of the room is: %.2lf\n", room_volume);
-    scanf("%c");
+    printf("The area of the floor is %.2lfm\n", floor_area);
+    printf("The amount of paint needed to paint the walls is: %.2lfm2\n", paint_needed);
+    printf("The volume of the room is: %.2lfm3\n", room_volume);
 
     return 0;
 }
